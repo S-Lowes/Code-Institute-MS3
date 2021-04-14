7d9a04752df853dfeb8ee7da71f0f742c4c41e98
S-Lowes <seblowes@btinternet.com>
2021-04-14 22:39:21 +0000

    (feat):Add Login functionality

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ def get_recipes():
     return render_template("recipes.html", recipes=recipes)
 
 
+# ===== REGISTER =====
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
@@ -47,6 +48,33 @@ def register():
         session["user"] = request.form.get("username").lower()
         flash("Registration successful, Welome to YUM!")
     return render_template("register.html")
+
+
+# ===== LOGIN =====
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in database
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches input
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
 
 
 # debug = false before submission
