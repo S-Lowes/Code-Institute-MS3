cd1b6004e1f3e761830714dba1747807b4a9419d
S-Lowes <seblowes@btinternet.com>
2021-04-16 23:15:51 +0000

    (tweak): Minor change to variable names in script.js

--- static/js/script.js
+++ static/js/script.js
@@ -17,8 +17,8 @@ add_more_ingredients.onclick = function () {
     var newField1 = document.createElement('input');
     newField1.setAttribute('type', 'text');
     newField1.setAttribute('class', 'form-control');
-    newField1.setAttribute('name', 'ingredient_input');
-    newField1.setAttribute('id', 'ingredient_input');
+    newField1.setAttribute('name', 'ingredient[]');
+    newField1.setAttribute('id', 'ingredient[]');
     newField1.setAttribute('placeholder', 'What ingredient?');
     newField1.setAttribute('required', '');
     ingredient.appendChild(newField1);
@@ -26,8 +26,8 @@ add_more_ingredients.onclick = function () {
     var newField2 = document.createElement('input');
     newField2.setAttribute('type', 'text');
     newField2.setAttribute('class', 'form-control');
-    newField2.setAttribute('name', 'measurement_input');
-    newField2.setAttribute('id', 'measurement_input');
+    newField2.setAttribute('name', 'measurement[]');
+    newField2.setAttribute('id', 'measurement[]');
     newField2.setAttribute('placeholder', 'How Much?');
     newField2.setAttribute('required', '');
     measurement.appendChild(newField2);
@@ -51,8 +51,8 @@ add_step.onclick = function () {
     var newField = document.createElement('textarea');
     newField.setAttribute('type', 'text');
     newField.setAttribute('class', 'form-control');
-    newField.setAttribute('name', 'step');
-    newField.setAttribute('id', 'steps');
+    newField.setAttribute('name', 'step[]');
+    newField.setAttribute('id', 'step[]');
     newField.setAttribute('placeholder', 'What do I do next?');
     newField.setAttribute('required', '');
     step.appendChild(newField);
