63c3bcaf398f72865f8fdca30e6528084d3e4bf1
S-Lowes <seblowes@btinternet.com>
2021-04-26 17:32:27 +0000

    (refactor):Change var to let in script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,21 +1,21 @@
 // ===== INGREDIENTS VARIABLES =====
 
-var ingredient = document.getElementById('ingredient');
-var measurement = document.getElementById('measurement');
-var amount = document.getElementById('amount');
-var add_more_ingredients = document.getElementById('add_more_ingredients');
-var remove_ingredients = document.getElementById('remove_ingredients');
+let ingredient = document.getElementById('ingredient');
+let measurement = document.getElementById('measurement');
+let amount = document.getElementById('amount');
+let add_more_ingredients = document.getElementById('add_more_ingredients');
+let remove_ingredients = document.getElementById('remove_ingredients');
 
 // ===== STEP VARIABLES =====
 
-var step = document.getElementById('step');
-var add_step = document.getElementById('add_step');
-var remove_step = document.getElementById('remove_step');
+let step = document.getElementById('step');
+let add_step = document.getElementById('add_step');
+let remove_step = document.getElementById('remove_step');
 
 // ===== ADD INGREDIENTS =====
 
 add_more_ingredients.onclick = function () {
-    var newField1 = document.createElement('input');
+    let newField1 = document.createElement('input');
     newField1.setAttribute('type', 'text');
     newField1.setAttribute('class', 'form-control');
     newField1.setAttribute('name', 'ingredient');
@@ -24,7 +24,7 @@ add_more_ingredients.onclick = function () {
     newField1.setAttribute('required', '');
     ingredient.appendChild(newField1);
 
-    var newField2 = document.createElement('input');
+    let newField2 = document.createElement('input');
     newField2.setAttribute('type', 'text');
     newField2.setAttribute('class', 'form-control');
     newField2.setAttribute('name', 'amount');
@@ -33,7 +33,7 @@ add_more_ingredients.onclick = function () {
     newField2.setAttribute('required', '');
     amount.appendChild(newField2);
 
-    var newField3 = document.createElement('input');
+    let newField3 = document.createElement('input');
     newField3.setAttribute('type', 'text');
     newField3.setAttribute('class', 'form-control');
     newField3.setAttribute('name', 'measurement');
@@ -44,9 +44,9 @@ add_more_ingredients.onclick = function () {
 }
 
 remove_ingredients.onclick = function () {
-    var ingredient_tags = ingredient.getElementsByTagName('input');
-    var amount_tags = amount.getElementsByTagName('input');
-    var measurement_tags = measurement.getElementsByTagName('input');
+    let ingredient_tags = ingredient.getElementsByTagName('input');
+    let amount_tags = amount.getElementsByTagName('input');
+    let measurement_tags = measurement.getElementsByTagName('input');
     if (ingredient_tags.length == 1 && measurement_tags.length == 1 && amount_tags.length == 1) {
 
     } else {
@@ -59,7 +59,7 @@ remove_ingredients.onclick = function () {
 // ===== ADD STEPS =====
 
 add_step.onclick = function () {
-    var newField = document.createElement('textarea');
+    let newField = document.createElement('textarea');
     newField.setAttribute('type', 'text');
     newField.setAttribute('class', 'form-control');
     newField.setAttribute('name', 'step');
@@ -70,7 +70,7 @@ add_step.onclick = function () {
 }
 
 remove_step.onclick = function () {
-    var step_tags = step.getElementsByTagName('textarea');
+    let step_tags = step.getElementsByTagName('textarea');
     if (step_tags.length == 1) {
 
     } else {
@@ -79,3 +79,17 @@ remove_step.onclick = function () {
 }
 
 //https://www.youtube.com/watch?v=MLBLsxcB3Dc
+
+/*
+
+let input = `<input type="text" class="form-control" name="ingredient" id="ingredient" placeholder="What ingredient?" required="">`;
+ingredient.innerHTML = input;
+
+let newField1 = document.createElement('input');
+newField1.setAttribute('type', 'text');
+newField1.setAttribute('class', 'form-control');
+newField1.setAttribute('name', 'ingredient');
+newField1.setAttribute('id', 'ingredient');
+newField1.setAttribute('placeholder', 'What ingredient?');
+newField1.setAttribute('required', '');
+*/
