4afa227e9f0166ed6f89751111ebac3f91b8d293
S-Lowes <seblowes@btinternet.com>
2021-04-30 02:48:35 +0000

    (fix):create_recipe.html now passes validation

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,13 @@
 // ===== INGREDIENTS VARIABLES =====
-let ingredient = document.getElementById('ingredient');
-let measurement = document.getElementById('measurement');
-let amount = document.getElementById('amount');
+let ingredients = document.getElementById('ingredients');
+let measurements = document.getElementById('measurements');
+let amounts = document.getElementById('amounts');
 let add_more_ingredients = document.getElementById('add_more_ingredients');
 let remove_ingredients = document.getElementById('remove_ingredients');
 
 // ===== STEP VARIABLES =====
 
-let step = document.getElementById('step');
+let steps = document.getElementById('steps');
 let add_step = document.getElementById('add_step');
 let remove_step = document.getElementById('remove_step');
 
@@ -16,28 +16,28 @@ let remove_step = document.getElementById('remove_step');
 add_more_ingredients.onclick = function () {
     let input1 = `<input type="text" class="form-control" name="ingredient"
         id="ingredient" placeholder="What ingredient?" required="">`;
-    ingredient.insertAdjacentHTML("beforeend", input1);
+    ingredients.insertAdjacentHTML("beforeend", input1);
 
     let input2 = `<input type="text" class="form-control" name="amount"
         id="amount" placeholder="How Much?" required="">`;
-    amount.insertAdjacentHTML("beforeend", input2);
+    amounts.insertAdjacentHTML("beforeend", input2);
 
     let input3 = `<input type="text" class="form-control" name="measurement"
         id="measurement" placeholder="What Measure?" required="">`;
-    measurement.insertAdjacentHTML("beforeend", input3);
+    measurements.insertAdjacentHTML("beforeend", input3);
 };
 
 remove_ingredients.onclick = function () {
-    let ingredient_tags = ingredient.getElementsByTagName('input');
-    let amount_tags = amount.getElementsByTagName('input');
-    let measurement_tags = measurement.getElementsByTagName('input');
+    let ingredient_tags = ingredients.getElementsByTagName('input');
+    let amount_tags = amounts.getElementsByTagName('input');
+    let measurement_tags = measurements.getElementsByTagName('input');
     if (ingredient_tags.length == 1 && measurement_tags.length == 1 &&
         amount_tags.length == 1) {
 
     } else {
-        ingredient.removeChild(ingredient_tags[(ingredient_tags.length) - 1]);
-        amount.removeChild(amount_tags[(amount_tags.length) - 1]);
-        measurement.removeChild(measurement_tags[(measurement_tags.length) - 1]);
+        ingredients.removeChild(ingredient_tags[(ingredient_tags.length) - 1]);
+        amounts.removeChild(amount_tags[(amount_tags.length) - 1]);
+        measurements.removeChild(measurement_tags[(measurement_tags.length) - 1]);
     }
 };
 
@@ -45,16 +45,16 @@ remove_ingredients.onclick = function () {
 
 add_step.onclick = function () {
 
-    let input4 = `<textarea type="text" class="form-control" name="step"
+    let input4 = `<textarea class="form-control" name="step"
         id="step" placeholder="What do I do next?" required=""></textarea>`;
-    step.insertAdjacentHTML("beforeend", input4);
+    steps.insertAdjacentHTML("beforeend", input4);
 };
 
 remove_step.onclick = function () {
-    let step_tags = step.getElementsByTagName('textarea');
+    let step_tags = steps.getElementsByTagName('textarea');
     if (step_tags.length == 1) {
 
     } else {
-        step.removeChild(step_tags[(step_tags.length) - 1]);
+        steps.removeChild(step_tags[(step_tags.length) - 1]);
     }
 };
